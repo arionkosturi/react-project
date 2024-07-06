ba3426c21c7511efd28cf13cba896db31c4a660b
arkotech <arionkosturi@gmail.com>
2024-07-06 16:26:22 +0200

    added delete functionality

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { FaTrash } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
 
 const api = axios.create({
     baseURL: 'http://localhost:3344/news/',
 });
+
 function Articles() {
     const [articles, setArticles] = useState([]);
     React.useEffect(() => {
@@ -11,26 +14,42 @@ function Articles() {
             setArticles(res.data);
         });
 
-        // return () => {
-        //   cleanup code if needed
-        // }
+        return () => {};
     }, []);
+    let show_after_deleted = () => {
+        api.get('/').then((res) => {
+            setArticles(res.data);
+        });
+    };
+
     return articles.map((article) => {
+        let handleDelete = () => {
+            api.delete(`/${article._id}`)
+                .then((response) => {
+                    show_after_deleted();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        };
+
         return (
             <div
                 onClick={() => console.log(article._id)}
-                className="container mx-auto border my-1"
+                className="flex container mx-auto border my-1"
                 key={article._id}
             >
-                <div className="flex p-2">
+                <div className="flex p-2 ">
                     <img className="w-1/3 my-2" src={article.imgUrl} />
-                    <div>
-                        <h1 className="font-bold mx-4 my-2  text-purple-400">
-                            {article.title}
-                        </h1>
-                        <p className="mx-4">{article.description}</p>
-                    </div>
+                    <h1 className="font-bold mx-4 my-2  text-purple-400">
+                        {article.title}
+                    </h1>
                 </div>
+                {/* Delete Button */}
+                <FaTrash
+                    onClick={handleDelete}
+                    className="mt-4 w-48 hover:text-red-600"
+                />
             </div>
         );
     });
